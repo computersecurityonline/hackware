4f7915cc0253cc05eb4dc5a25235418324ba79f9
myuserinterface <57772346+myuserinterface@users.noreply.github.com>
2020-06-07 01:57:01 +0300

    Create READ_ME.lua

---
+++ READ_ME.lua
@@ -0,0 +1,44 @@
+--[[
+Hey!
+
+My name is Andrew and I am a 14 year old ROBLOX developer who made this anti-cheat alone to hopefully keep your game safe from exploiters.
+ROBLOX Profile : https://www.roblox.com/users/288919079/profile
+
+Features : 
+
+- Auto-Updating 
+Description : Whenever hackware updates, this page always updates and the script automatically updates.
+
+- Instant Detection
+Description : This can detect exploiters the second they execute something we can detect.
+
+- Amazing Detection Methods
+Description : We use detection methods to make sure we are updated with the latest security technology.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+What it detects : 
+
+- WalkSpeed changes 
+Description : A WalkSpeed is the LocalPlayer (player)'s speed. It's always set to 16 and if it goes higher the player automatically gets 
+kicked from the game due to exploiting.
+
+- JumpPower changes
+Description : A JumpPower is the LocalPlayer (player)'s JumpPower. It's always set to 50 and if it goes higher the player gets kicked.
+
+- HipHeight changes
+Description : HipHeight determines the distance (in studs) off the ground. Normally set to 2. If it goes higher than the player gets kicked.
+
+- Teleporting
+Description : Exploiters can teleport to locations if using a exploit. So here is a massive patch. There are over 5 checks currently.
+
+- Flying 
+Description : Exploits can fly in your game using basic scripts, so here is a patch. 2 are set.
+
+- Anti-Asset Stealing
+Description : This detects when a exploiter uses a common attack where he can open explorer GUIs where he can copy your whole game and view scripts. 
+This is a small bypass for it
+
+
+
+]]
